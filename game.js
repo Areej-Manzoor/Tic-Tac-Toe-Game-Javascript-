8b1399bfd6a99f317dce0897a6a315709c4f53e6
Areej-Manzoor <areejmanzoor456@gmail.com>
2021-04-17 12:58:02 -0500

    all winning scenerios done

--- game.js
+++ game.js
@@ -30,7 +30,7 @@ function yourTurn(e)
  
  }
  var b1=document.getElementById("b1").value;
- var b=document.getElementById("b2").value;
+ var b2=document.getElementById("b2").value;
  var b3=document.getElementById("b3").value;
  var b4=document.getElementById("b4").value;
  var b5=document.getElementById("b5").value;
@@ -39,16 +39,15 @@ function yourTurn(e)
  var b8=document.getElementById("b8").value;
  var b9=document.getElementById("b9").value;
  
-//  if((b1=='X' && b2=='X' && b3=='X') || (b1=='O' && b2=='O' && b3=='O'))
-// {
-//     setTimeout (function(){
+ if((b1=='X' && b2=='X' && b3=='X') || (b1=='O' && b2=='O' && b3=='O'))
+{
+
          
-//         document.getElementById("tic-tac-block").style.display ='none';
-//         document.getElementById("announcement").innerHTML=e.value+" WON";
-     
-//     },1000);
-// }
- if((b4=='X' && b5=='X' && b6=='X') || (b4=='O' && b5=='O' && b6=='O'))
+        document.getElementById("tic-tac-block").style.display ='none';
+        document.getElementById("announcement").innerHTML=e.value+" WON";
+
+}
+ else if((b4=='X' && b5=='X' && b6=='X') || (b4=='O' && b5=='O' && b6=='O'))
 {
 
          
@@ -56,15 +55,7 @@ function yourTurn(e)
         document.getElementById("announcement").innerHTML=e.value+" WON";
      
 }
-else if((b1=='X' && b2=='X' && b3=='X') || (b1=='O' && b2=='O' && b3=='O'))
-{
 
-         
-        document.getElementById("tic-tac-block").style.display ='none';
-        document.getElementById("announcement").innerHTML=e.value+" WON";
-     
-  
-}
  else if((b7=='X' && b8=='X' && b9=='X') || (b7=='O' && b8=='O' && b9=='O'))
 {
     
